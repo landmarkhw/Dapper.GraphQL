7e5517f24cc983ad5720b9751d9a643bdda507a1
Ben McCallum <ben.mccallum@live.com.au>
2018-03-28 04:53:18 +0200

    Adds async support (#5)
    * Added support for async query execution, and a test that confirms it produces the same result as sync execution.
    
    * Fixing the position of braces to make the PR diff clearer.
    
    * Making the PR diff clearer again.

--- Dapper.GraphQL.Test/GraphQLTests.cs
+++ Dapper.GraphQL.Test/GraphQLTests.cs
@@ -140,6 +140,124 @@ query {
             Assert.True(fixture.JsonEquals(expectedJson, json));
         }
 
+        [Fact(DisplayName = "Full peopleAsync query should succeed")]
+        public async Task FullPeopleAsyncQuery()
+        {
+            var json = await fixture.QueryGraphQLAsync(@"
+query {
+    peopleAsync {
+        id
+        firstName
+        lastName
+        emails {
+            id
+            address
+        }
+        phones {
+            id
+            number
+            type
+        }
+        supervisor {
+            id
+            firstName
+            lastName
+        }
+        careerCounselor {
+            id
+            firstName
+            lastName
+        }
+    }
+}");
+
+            var expectedJson = @"
+{
+  ""data"": {
+    ""peopleAsync"": [
+      {
+        ""id"": 1,
+        ""firstName"": ""Hyrum"",
+        ""lastName"": ""Clyde"",
+        ""emails"": [
+          {
+            ""id"": 1,
+            ""address"": ""hclyde@landmarkhw.com""
+          }
+        ],
+        ""phones"": [],
+        ""supervisor"": null,
+        ""careerCounselor"": null
+      },
+      {
+        ""id"": 2,
+        ""firstName"": ""Doug"",
+        ""lastName"": ""Day"",
+        ""emails"": [
+          {
+            ""id"": 2,
+            ""address"": ""dday@landmarkhw.com""
+          },
+          {
+            ""id"": 3,
+            ""address"": ""dougrday@gmail.com""
+          }
+        ],
+        ""phones"": [
+          {
+            ""id"": 1,
+            ""number"": ""8011234567"",
+            ""type"": 3
+          }
+        ],
+        ""supervisor"": null,
+        ""careerCounselor"": {
+          ""id"": 1,
+          ""firstName"": ""Hyrum"",
+          ""lastName"": ""Clyde""
+        }
+      },
+      {
+        ""id"": 3,
+        ""firstName"": ""Kevin"",
+        ""lastName"": ""Russon"",
+        ""emails"": [
+          {
+            ""id"": 4,
+            ""address"": ""krusson@landmarkhw.com""
+          }
+        ],
+        ""phones"": [
+          {
+            ""id"": 2,
+            ""number"": ""8019876543"",
+            ""type"": 3
+          },
+          {
+            ""id"": 3,
+            ""number"": ""8011111111"",
+            ""type"": 1
+          }
+        ],
+        ""supervisor"": {
+          ""id"": 1,
+          ""firstName"": ""Hyrum"",
+          ""lastName"": ""Clyde""
+        },
+        ""careerCounselor"": {
+          ""id"": 2,
+          ""firstName"": ""Doug"",
+          ""lastName"": ""Day""
+        }
+      }
+    ]
+  }
+}";
+
+            Assert.True(fixture.JsonEquals(expectedJson, json));
+        }
+
+
         [Fact(DisplayName = "Full person query should succeed")]
         public async Task FullPersonQuery()
         {
