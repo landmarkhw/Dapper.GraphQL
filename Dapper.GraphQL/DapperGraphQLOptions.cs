1e7b92f0bcfe8de563872dbed8d843f05e3f3d66
Per Liedman <per@liedman.net>
2018-11-01 04:14:44 +0100

    Add non-generic versions of service registration methods (#31)
    * Add non-generic versions of service registration methods
    
    * Add XML docs

--- Dapper.GraphQL/DapperGraphQLOptions.cs
+++ Dapper.GraphQL/DapperGraphQLOptions.cs
@@ -1,4 +1,7 @@
-﻿using GraphQL.Types;
+﻿using System;
+using System.Reflection;
+using GraphQL;
+using GraphQL.Types;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Dapper.GraphQL
@@ -29,6 +32,24 @@ namespace Dapper.GraphQL
         }
 
         /// <summary>
+        /// Adds a GraphQL query builder to the container.
+        /// </summary>
+        /// <param name="modelType">The model type to be queried.</param>
+        /// <param name="queryBuilderType">The query builder class, must implement IQueryBuilder<modelType></param>
+        /// <returns>The GraphQLOptions object.</returns>
+        public DapperGraphQLOptions AddQueryBuilder(Type modelType, Type queryBuilderType)
+        {
+            var queryBuilderInterface = typeof(IQueryBuilder<>).MakeGenericType(modelType);
+            if (!queryBuilderType.IsConcrete() || !queryBuilderInterface.IsAssignableFrom(queryBuilderType))
+            {
+                throw new ArgumentException($"QueryBuilder type must be concrete and implement IQueryBuilder<{modelType.Name}>.");
+            }
+
+            serviceCollection.Add(new ServiceDescriptor(queryBuilderInterface, queryBuilderType, ServiceLifetime.Singleton));
+            return this;
+        }
+
+        /// <summary>
         /// Adds a GraphQL schema to the container.
         /// </summary>
         /// <typeparam name="TGraphSchema">The schema type to be mapped.</typeparam>
@@ -40,6 +61,22 @@ namespace Dapper.GraphQL
         }
 
         /// <summary>
+        /// Adds a GraphQL schema to the container.
+        /// </summary>
+        /// <param name="graphSchemaType">The schema type to be mapped, must implement ISchema.</param>
+        /// <returns>The GraphQLOptions object.</returns>
+        public DapperGraphQLOptions AddSchema(Type graphSchemaType)
+        {
+            if (!graphSchemaType.IsConcrete() || !typeof(ISchema).IsAssignableFrom(graphSchemaType))
+            {
+                throw new ArgumentException("Type must be concrete and implement ISchema.");
+            }
+
+            serviceCollection.Add(new ServiceDescriptor(graphSchemaType, graphSchemaType, ServiceLifetime.Singleton));
+            return this;
+        }
+
+        /// <summary>
         /// Adds a GraphQL type to the container.
         /// </summary>
         /// <typeparam name="TGraphType">The model type to be mapped.</typeparam>
@@ -47,6 +84,22 @@ namespace Dapper.GraphQL
         public DapperGraphQLOptions AddType<TGraphType>() where TGraphType : class, IGraphType
         {
             serviceCollection.AddSingleton<TGraphType>();
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a GraphQL type to the container.
+        /// </summary>
+        /// <param name="type">The model type to be mapped, must implement IGraphType.</param>
+        /// <returns>The GraphQLOptions object.</returns>
+        public DapperGraphQLOptions AddType(Type type)
+        {
+            if (!type.IsConcrete() || !typeof(IGraphType).IsAssignableFrom(type))
+            {
+                throw new ArgumentException("Type must be concrete and implement IGraphType.");
+            }
+
+            serviceCollection.Add(new ServiceDescriptor(type, type, ServiceLifetime.Singleton));
             return this;
         }
     }
